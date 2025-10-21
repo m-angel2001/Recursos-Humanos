b4e8a57aa1f1861d06e9c9993fe631fabeff0df2
m-angel2001 <romerohernandezmiguelangel39@gmail.com>
2025-10-20 20:47:16 -0600

    Inicio del backend, creacion de Spring boot, configuracion y creacion de entidad

---
+++ recursos_humanos/rh/src/main/java/gm/rh/modelo/Empleado.java
@@ -0,0 +1,80 @@
+package gm.rh.modelo;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "Empleado")
+// CLASE //
+public class Empleado {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long idEmpleado;
+
+    private String nombre;
+    private String departamento;
+    private Double sueldo;
+    // Constructores
+
+    public Empleado() {
+
+    }
+    public Empleado(Long idEmpleado, String nombre, String departamento, Double sueldo) {
+        this.idEmpleado = idEmpleado;
+        this.nombre = nombre;
+        this.departamento = departamento;
+        this.sueldo = sueldo;
+
+    }
+
+    public Empleado(String nombre, String departamento, Double sueldo) {
+        this.nombre = nombre;
+        this.departamento = departamento;
+        this.sueldo = sueldo;
+
+    }
+
+    // METODOS GET Y SET
+
+    public Long getidEmpleado(Long idEmpleado) {
+        return idEmpleado;
+    }
+
+    public void setidEmpleado(Long idEmpleado) {
+        this.idEmpleado = idEmpleado;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public String getdepartamento() {
+        return departamento;
+    }
+
+    public void setdepartamento(String departamento) {
+        this.departamento = departamento;
+    }
+
+    public Double getsueldo() {
+        return sueldo;
+    }
+
+    public void setsueldo(Double sueldo) {
+        this.sueldo = sueldo;
+    }
+
+    // to String
+
+    @Override
+    public String toString() {
+        return "Empleado{" +
+                "idEmpleado" + idEmpleado +
+                ", nombre" + nombre + '\'' +
+                ", departamento= " + departamento + '\'' +
+                ", sueldo= " + sueldo +
+                '}';
+
+
+    }
+}
