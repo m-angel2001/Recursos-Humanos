f6b1a658a07eb9b8a30c9da323c8634497dd4d4b
m-angel2001 <romerohernandezmiguelangel39@gmail.com>
2025-10-20 20:59:53 -0600

    Se agrego la capa de datos

---
+++ recursos_humanos/rh/src/main/java/gm/rh/repositorio/EmpleadoRepositorio.java
@@ -0,0 +1,10 @@
+package gm.rh.repositorio;
+
+import gm.rh.modelo.Empleado;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EmpleadoRepositorio extends JpaRepository<Empleado, Long> {
+
+}
