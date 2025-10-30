f7eab65d97907d3f1567509430cadf2dd2d69e57
m-angel2001 <romerohernandezmiguelangel39@gmail.com>
2025-10-29 20:03:23 -0600

    Barra de navegacion

--- recursos_humanos_app/src/App.jsx
+++ recursos_humanos_app/src/App.jsx
@@ -2,13 +2,17 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import ListadoEmpleados from './empleados/ListadoEmpleados'
+import Navegacion from './plantilla/Navegacion'
 
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
+    <>
+    <Navegacion/>
     <ListadoEmpleados/>
+    </>
   )
 }
 
