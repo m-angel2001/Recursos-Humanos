9ccc3767c7eca6ce8d35269e251e2ce348cca356
m-angel2001 <romerohernandezmiguelangel39@gmail.com>
2025-11-03 17:43:20 -0600

    Metodo agregar nuevo empleado desde la interfaz

--- recursos_humanos_app/src/empleados/AgregarEmpleado.jsx
+++ recursos_humanos_app/src/empleados/AgregarEmpleado.jsx
@@ -1,26 +1,55 @@
-import React from "react";
+import axios from "axios";
+import React, { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 
 export default function AgregarEmpleado() {
+
+  const[empleado, setEmpleado]=useState({
+    nombre:"", 
+    departamento:"",
+    sueldo:""
+  })
+  const{nombre, departamento, sueldo} = empleado
+  const onInputChange = (e) => { //Se expanden los atributos
+    setEmpleado({...empleado, [e.target.name]: e.target.value})
+
+  }
+  let navegacion = useNavigate();
+  
+  const onSubmit = async (e) => {
+    e.preventDefault();
+    const urlBase = "http://localhost:8080/api/empleados";
+    await axios.post(urlBase, empleado);
+
+    //Redirigir a la pagina de inicio
+navegacion('/');
+  }
 return (
 <div className="container">
 
     <div className="container text-center" style={{margin: "30px"}}>
   <h3>Agregar Empleado</h3>
     </div>
-    <form>
+    <form onSubmit={(e) => onSubmit(e)}>
   <div className="mb-3">
     <label htmlFor="nombre" className="form-label">Nombre</label>
-    <input type="text" className="form-control" id="nombre" name="nombre" required={true}/>
+    <input type="text" className="form-control" id="nombre" name="nombre" required={true}
+    value={nombre} onChange={(e) => onInputChange(e)} />
     </div>
 
     <div className="mb-3">
     <label htmlFor="departamento" className="form-label">Departamento</label>
-    <input type="text" className="form-control" id="departamento" name="departamento" required={true}/>
+    <input type="text" className="form-control" id="departamento" name="departamento" required={true}
+    value={departamento} onChange={(e) => onInputChange(e)}
+    />
     </div>
 
     <div className="mb-3">
     <label htmlFor="sueldo" className="form-label">Sueldo</label>
-    <input type="number" step="any" className="form-control" id="sueldo" name="sueldo" />
+    <input type="number" step="any" className="form-control" id="sueldo" name="sueldo" 
+    value={sueldo} onChange={(e) => onInputChange(e)}
+    />
     </div>
 
     <div className="text-center">
@@ -35,4 +64,5 @@ return (
 </div>
 )
 }
+
 
